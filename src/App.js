264649708dd129d129ff4c9ba69ef5392db47653
Mikita Aleksandrovich <45469739+MikitaAleksandrovich@users.noreply.github.com>
2020-03-10 16:54:10 +0300

    Carry out arrays of common english words to assets

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import { complexText } from './components/CheckText/ComplexText';
+import { simpleText } from './components/CheckText/SimpleText';
 
 
 function App() {
@@ -8,7 +8,10 @@ function App() {
   return (
     <div className="extension">
       <h1>This text: "Translate words while reading an article or watching your favourite movie." is simple for
-        <h2>{complexText("Translate words while reading an article or watching your favourite movie.")}</h2>
+        <p>{simpleText("Translate words while reading an article or watching your favourite movie.")}</p>
+      </h1>
+      <h1>This text: "We help to learn the language for free and comfortably, doing what you love - watching TV shows, reading books, or listening to music." is simple for
+        <p>{simpleText("We help to learn the language for free and comfortably, doing what you love - watching TV shows, reading books, or listening to music.")}</p>
       </h1>
     </div>
   );
