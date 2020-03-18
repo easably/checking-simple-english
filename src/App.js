088fbfb4cfd8a52c403a5d2feb4d87a364860461
pavladan <fatalfenker@gmail.com>
2020-03-18 12:57:21 +0300

    Getting DOM text in popup

--- src/App.js
+++ src/App.js
@@ -13,6 +13,11 @@ export default class App extends Component {
   }
 
   componentDidMount() {
+		getDOMText().then(text=>{
+			this.setState({
+				bodyText: text
+			})
+		})
     document.addEventListener("mouseup", this.getText);
   }
 
@@ -39,17 +44,15 @@ export default class App extends Component {
   
   render() {
 
-    getDOMText();
-
     const { selectedText, bodyText } = this.state;
 
     return (
 
       <div className="Extension">
 
-      {/* <h1>This page covers {simple_1000_Text(bodyText)} of common 1000 English words 
+      <h1>This page covers {simple_1000_Text(bodyText)} of common 1000 English words 
                                     and {simple_100_Text(bodyText)} of common 100 English words.
-      </h1> */}
+      </h1>
 
         <h1 style={{ color: "red" }}>Statistics of page's body will go here: </h1>
 
--- src/components/GetDOMText/getDOMText.js
+++ src/components/GetDOMText/getDOMText.js
@@ -1,17 +1,19 @@
 /*global chrome*/
 
+export const getDOMText = async () => {
+  return new Promise((resolve, reject) => {
+    chrome.tabs.query({ active: true }, tabs => {
+      var tab = tabs[0];
 
-export const getDOMText = () => {
-
-  chrome.tabs.query({active: true}, (tabs) => {
-
-  var tab = tabs[0];
-
-  chrome.tabs.executeScript(tab.id, {
-    code: 'document.querySelector("body").innerText'
-  }, (results) => {
-    console.log(results);
+      chrome.tabs.executeScript(
+        tab.id,
+        {
+          code: 'document.querySelector("body").innerText'
+        },
+        results => {
+          resolve(results && results[0]);
+        }
+      );
+    });
   });
-  
-});
 };
