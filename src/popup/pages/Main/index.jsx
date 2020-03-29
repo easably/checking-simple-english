46c8b3cfb8593aef4a3c3f6705a39b0c7745e845
Mikita Aleksandrovich <45469739+MikitaAleksandrovich@users.noreply.github.com>
2020-03-29 22:57:14 +0300

    Improve perfomance by refactor main with hooks

--- src/popup/pages/Main/index.jsx
+++ src/popup/pages/Main/index.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 
 import { checkTextSimple } from '../../helpers/checkTextSimple/checkTextSimple';
 import { checkWordsNum } from '../../helpers/checkWordsNum/checkWordsNum';
@@ -11,45 +11,38 @@ import { my_Vocabulary } from '../../../assets/myVocabulary/myVocabulary';
 import "./Main.scss";
 import Spinner from '../../components/Spinner';
 
+const main = () => {
 
-export default class Main extends Component {
+  const [bodyText, setBodyText] = useState('');
 
-  state = {
-    bodyText: '',
-    loading: true,
-  }
+  const [loading, setLoading] = useState(true);
 
-  componentDidMount() {
+  useEffect( () => {
+    getDOMText()
+      .then( text => {
+        setBodyText(text);
+        setLoading(!loading);
+      });
+ },[]);
 
-    getDOMText().then(text => {
-      this.setState({
-        bodyText: text,
-        loading: false,
-      })
-    });
-  }
+ if(loading) {
+   return <Spinner/>
+ }
 
-  render() {
-
-    const { bodyText, loading } = this.state;
-
-    if (loading) {
-      return <Spinner />
-    }
-    
-    return (
-      <div style={{ color: "#fff" }}>
-        <h1>Vocabulary in this page: </h1>
-        <h3>Words from "Common Thousand": <span style={{ color: "red" }}>{checkWordsNum(bodyText, common_1000_EnglishWords)}</span>, <br />
-        covers <span style={{ color: "red" }}>{checkTextSimple(bodyText, common_1000_EnglishWords)}</span> of page's content.
-    </h3>
-        <h3>Words from "Common Hundred": <span style={{ color: "red" }}>{checkWordsNum(bodyText, common_100_EnglishWords)}</span>, <br />
-        covers <span style={{ color: "red" }}>{checkTextSimple(bodyText, common_100_EnglishWords)}</span> of page's content.
-    </h3>
-        <h3>Words from "My Vocabulary": <span style={{ color: "red" }}>{checkWordsNum(bodyText, my_Vocabulary)}</span>,
-        covers <span style={{ color: "red" }}>{checkTextSimple(bodyText, my_Vocabulary)}</span>.
-    </h3>
-      </div>
-    )
-  }
+  return (
+    <div style={{ color: "#fff" }}>
+    <h1>Vocabulary in this page: </h1>
+    <h3>Words from "Common Thousand": <span style={{ color: "red" }}>{checkWordsNum(bodyText, common_1000_EnglishWords)}</span>, <br />
+    covers <span style={{ color: "red" }}>{checkTextSimple(bodyText, common_1000_EnglishWords)}</span> of page's content.
+</h3>
+    <h3>Words from "Common Hundred": <span style={{ color: "red" }}>{checkWordsNum(bodyText, common_100_EnglishWords)}</span>, <br />
+    covers <span style={{ color: "red" }}>{checkTextSimple(bodyText, common_100_EnglishWords)}</span> of page's content.
+</h3>
+    <h3>Words from "My Vocabulary": <span style={{ color: "red" }}>{checkWordsNum(bodyText, my_Vocabulary)}</span>,
+    covers <span style={{ color: "red" }}>{checkTextSimple(bodyText, my_Vocabulary)}</span>.
+</h3>
+  </div>
+  );
 };
+
+export default main;
